4a48bc7ae1fe3d4f1d98b6805e070a948f6aab03
Guillaume Bourgarel <guillaume.bourgar>
2020-11-05 15:36:00 +0100

    motion_planner updated

--- script/motion_planner.py
+++ script/motion_planner.py
@@ -4,10 +4,8 @@ class MotionPlanner:
     self.ps = ps
 
   def solveBiRRT (self, maxIter = float("inf")):
-    print ("Method solveBiRRT is not implemented yet")
     self.ps.prepareSolveStepByStep ()
     finished = False
-
     # In the framework of the course, we restrict ourselves to 2 connected components.
     nbCC = self.ps.numberConnectedComponents ()
     if nbCC != 2:
@@ -17,9 +15,22 @@ class MotionPlanner:
     while True:
       #### RRT begin
       newConfigs = list ()
-
+      config = self.robot.shootRandomConfig()
+      for i in range(self.ps.numberConnectedComponents()):
+        nearestConfig, _ = self.ps.getNearestConfig(config, i)
+        _, pathId, _ = self.ps.directPath(nearestConfig, config, True)
+        q = self.ps.configAtParam(pathId, self.ps.pathLength(pathId))
+        newConfigs.append(q)
+        self.ps.addConfigToRoadmap(q)
+        self.ps.addEdgeToRoadmap(nearestConfig, q, pathId, True)
       ## Try connecting the new nodes together
       for i in range (len(newConfigs)):
+        if self.ps.numberConnectedComponents()!=2:
+          break
+        q_list = self.ps.nodesConnectedComponent(1-i)
+        for q in q_list:
+          if self.ps.directPath(newConfigs[i], q, True)[0]:
+            self.ps.addEdgeToRoadmap(newConfigs[i], q, self.ps.directPath(newConfigs[i], q, True)[1], True)
         pass
       #### RRT end
       ## Check if the problem is solved.
