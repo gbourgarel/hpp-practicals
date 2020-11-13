5d13d4d28d58fdf66cdb420d9ed6112ead00b3da
Guillaume Bourgarel <guillaume.bourgar>
2020-11-13 08:54:26 +0100

    commit

--- script/solve_by_hand_in_box.py
+++ script/solve_by_hand_in_box.py
@@ -15,6 +15,26 @@ def generatePath(q_from, edgeName, q=None):
         if not res: continue
         return pid, q1
 
+def generatePathTwoEdges(q_from, edgeName1, edgeName2, q):
+    trial = 0
+    while True:
+        print('trial {0}'.format(trial))
+        trial += 1
+        q_ = robot.shootRandomConfig()
+        res, q1, err = graph.generateTargetConfig(edgeName1, q_from, q_)
+        if not res: continue
+        res, msg = robot.isConfigValid(q1)
+        if not res: continue
+        res, pid1, msg = ps.directPath(q_from, q1, True)
+        if not res: continue
+        res, q2, err = graph.generateTargetConfig(edgeName1, q1, q)
+        if not res: continue
+        res, msg = robot.isConfigValid(q2)
+        if not res: continue
+        res, pid2, msg = ps.directPath(q1, q2, True)
+        if not res: continue
+        return pid1, pid2, q1, q2
+
 v=vf.createViewer()
 pp=PathPlayer(v)
 paths = []
